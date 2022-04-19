ef6e33ce4d1a99dd57d921fd2c8058a0a00b5cf5
Jiwon <raipen@knu.ac.kr>
2022-04-20 01:08:16 +0900

    update counting longest path

--- 0330/main3.cpp
+++ 0330/main3.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 #include <vector>
-#define newMatrix(n,s,i) vector<vector<int> > n(s, vector<int>(s , i))
+#define newMatrix(n,s) vector<vector<int> > n(s, vector<int>(s , 0))
 using namespace std;
 
 int main() {
-	int N, temp, m = 0;
-	cin >> N;
-	newMatrix(origin, N + 1,0);
-	newMatrix(cost, N + 1,0);
-	newMatrix(path, N,0);
-	newMatrix(cnt, N+1,1);
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j <= i; j++)
-			cin >> origin[i][j];
-	for (int i = N - 1; i >= 0; i--)
-		for (int j = 0; j <= i; j++) {
-			path[i][j] = cost[i + 1][j] > cost[i + 1][j + 1] ? j : j + 1;
-			cnt[i][j] = cost[i + 1][j] == cost[i + 1][j + 1]?cnt[i+1][j]+cnt[i+1][j+1]:cnt[i+1][path[i][j]];
-			cost[i][j] = origin[i][j] + cost[i + 1][path[i][j]];
-		}
-	cout << cost[0][0] << endl;
-	for (int i = 0, cur = 0; i < N; cur = path[i][cur], i++)
-		cout << origin[i][cur] << " ";
-	cout<<endl <<cnt[0][0]<<endl;
+   int N, temp, m = 0;
+   cin >> N;
+   newMatrix(origin, N + 1);
+   newMatrix(cost, N + 1);
+   newMatrix(path, N);
+   newMatrix(cnt, N);
+   for (int i = 0; i < N; i++)
+      for (int j = 0; j <= i; j++)
+         cin >> origin[i][j];
+   for (int i = N - 1; i >= 0; i--)
+      for (int j = 0; j <= i; j++) {
+         path[i][j] = cost[i + 1][j] > cost[i + 1][j + 1] ? j : j + 1;
+         cnt[i][j] = i==N-1?1:cost[i + 1][j] == cost[i + 1][j + 1] ? cnt[i + 1][j] + cnt[i + 1][j + 1] : cnt[i + 1][path[i][j]];
+         cost[i][j] = origin[i][j] + cost[i + 1][path[i][j]];
+      }
+   cout << cost[0][0] << endl;
+   for (int i = 0, cur = 0; i < N; cur = path[i][cur], i++)
+      cout << origin[i][cur] << " ";
+   cout << endl << cnt[0][0] << endl;
 }
